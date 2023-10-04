d6292739cd5aa3c338386b2e08d0cd9317fda62c
Wagner C. Vaz <wagner.tech89@hotmail.com>
2023-10-04 15:49:11 -0300

    Navegação da página pelo id, cada notícia redireciona para sua página espefícifca

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "aaa",
+    "title": "Após longos anos, Luke Skywalker decide treinar padawans",
+    "description": "Padawans ficam eufóricos ao saberem da grande novidade sobre nova temporada de treinamentos para quem deseja ser um futuro Jedi.",
+    "photoCover": "https://e0.pxfuel.com/wallpapers/724/541/desktop-wallpaper-luke-skywalker-star-wars-the-last-jedi-2017-1440p-resolution-background-and-luke-skywalker-cool-thumbnail.jpg"
+  },
+  {
+    "id": "2",
+    "photo": "",
+    "title": "Ahsoka pede reunião com cúpula da resistência para criar novo conselho Jedi.",
+    "description": "Ahsoka deseja criar um novo Conselho Jedi para promover valores de equilíbrio, compaixão e aceitação. Ela busca uma liderança mais inclusiva, aberta à diversidade de perspectivas, e está comprometida em reconstruir a Ordem Jedi com base no aprendizado do passado, evitando os erros antigos. Seu objetivo é restaurar a confiança na Força e na capacidade dos Jedi de servir como guardiões da paz na galáxia.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZ3tbvBlRM5ar7i70Cx5Gi9lA9recQmh_mw&usqp=CAU"
+  },
+  {
+    "id": "3",
+    "photo": "",
+    "title": "Finn alcança grandes feitos em sua nova jornada.",
+    "description": "Finn realizou grandes feitos em sua nova jornada ao se tornar um corajoso herói da Resistência. Ele superou seu passado como stormtrooper da Primeira Ordem, mostrando coragem e determinação na luta contra o mal. Finn provou ser um aliado leal, contribuindo para vitórias cruciais e inspirando outros com seu compromisso em defender a liberdade e a justiça na galáxia.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRKnRcyeYAO82gZHUoxHr4Fh-2I-9jZIyK4A&usqp=CAU"
+  },
+  {
+    "id": "4",
+    "photo": "",
+    "title": "Rey parte em busca de novas crianças sensitivas à Força.",
+    "description": "Rey embarcou em uma nova jornada para encontrar crianças sensitivas à Força e oferecer a elas orientação e treinamento. Com sua experiência como a última Jedi, ela busca preservar o legado dos Jedi e garantir um futuro mais luminoso, cultivando o potencial dessas crianças para equilibrar a Força e trazer esperança à galáxia.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtv70Ofp6zMmocgEq4IDeYd10Nc6Bsc8v8Rw&usqp=CAU"
+  }
+]
